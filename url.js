8187ed595d2713d57e7e4539a66ae5e4f90ae414
Sai-Teja-Ambati <ambatisaiteja123@gmail.com>
2024-06-13 14:49:56 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,3 +1,3 @@
 //module.exports = `mongodb://localhost:27017`
-module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/"
-//module.exports="mongodb+srv://ambatisaiteja:admin@cluster0.xjauk0g.mongodb.net/"
+//module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/"
+module.exports="mongodb+srv://ambatisaiteja:admin@cluster0.xjauk0g.mongodb.net/"
